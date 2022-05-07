d2e40a7002f1388ad51e202ed756ef3300683867
1 <koreanka937@gmail.com>
2022-05-08 00:02:15 +0500

    Simple React List

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
+
+class App extends Component {
+	state = {
+		characters: [],
+	};
+
+	removeCharacter = (index) => {
+		const {characters} = this.state;
+		this.setState({
+			characters: characters.filter((character, i) => {
+				return i !== index;
+			}),
+		});
+	};
+
+	handleSubmit = (character) => {
+		this.setState({characters: [...this.state.characters, character]})
+	};
+
+	render() {
+		const { characters } = this.state;
+		return (
+			<div className="container">
+				<h1>Simple React List</h1>
+				<p>Add a character with a name and a job to the table.</p>
+				<h3>Add New</h3>
+				<Form handleSubmit={this.handleSubmit} />
+				<h3>List</h3>
+				<Table characterData={characters} removeCharacter={this.removeCharacter} />
+			</div>
+		);
+	}
+}
+
+export default App;
